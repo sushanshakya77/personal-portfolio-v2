b926f08d6e57534c077d54f63f04468cf1bf36fd
sushanshakya77 <sushanshakya77@gmail.com>
2024-09-21 15:58:35 +0545

    fix: moved inside dir

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Navbar from "./_components/Navbar";
+import Navbar from "./_components/navbar/Navbar";
 import { useEffect } from "react";
 
 export default function Home() {
