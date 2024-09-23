74a08c2bd1ea498eb922677d62d6f01db4259e00
sushanshakya77 <sushanshakya77@gmail.com>
2024-09-23 22:43:58 +0545

    feat: constants for recent projects

---
+++ src/constants/home/recentProjects.tsx
@@ -0,0 +1,20 @@
+export const recentProjects = [
+  {
+    id: 1,
+    name: "Veel",
+    smallIcon: "/workLogo/veel/veel.webp",
+    snippetImage: "/workLogo/veel/veelSnippet.webp",
+    textIcon: "/workLogo/veel/veelText.png",
+    description:
+      "Focused on developing, managing and optimizing web applications for the in-house application in attempt to revolutionalize video marketing through power of authentic content.",
+  },
+  {
+    id: 2,
+    name: "Travelog",
+    smallIcon: "/workLogo/travelog/travelogSmall.png",
+    snippetImage: "/workLogo/travelog/travelogSnippet.png",
+    textIcon: "/workLogo/travelog/travelogText.png",
+    description:
+      "Personal travel app for travellers, with integrated maps along with trip planners and many more.",
+  },
+] as const;
