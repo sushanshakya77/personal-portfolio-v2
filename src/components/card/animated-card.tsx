dbfc2d34e0ed2b056597fdf0b18e32b7b2fe9bd2
sushanshakya77 <sushanshakya77@gmail.com>
2024-09-29 19:45:14 +0545

    fix: hover follow through animation on card

--- src/components/card/animated-card.tsx
+++ src/components/card/animated-card.tsx
@@ -1,5 +1,5 @@
-import React, { PropsWithChildren, ReactNode } from "react";
-import { twMerge } from "tailwind-merge";
+import { cn } from "@/src/libs/utils";
+import React, { PropsWithChildren } from "react";
 import { Card } from "./card";
 
 type ICardProps = {
@@ -19,8 +19,8 @@ export const AnimatedCard = ({
 }: ICardProps) => {
   return (
     <Card
-      className={twMerge(
-        "rounded-xl hover:shadow-[0px_0px_40px_0px_rgb(36,36,36)] flex flex-col overflow-hidden duration-1000 will-change-auto hover:scale-105"
+      className={cn(
+        "rounded-xl flex flex-col overflow-hidden duration-1000 will-change-auto hover:shadow-[0px_0px_40px_0px_rgb(36,36,36)]"
       )}
       style={{
         height: "505px",
--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,5 +1,7 @@
-import React, { PropsWithChildren, ReactNode } from "react";
-import { twMerge } from "tailwind-merge";
+"use client";
+import { useMousePosition } from "@/src/hooks/misc/useMousePosition";
+import { cn } from "@/src/libs/utils";
+import React, { PropsWithChildren, useCallback, useRef } from "react";
 
 type ICardProps = {
   className?: HTMLDivElement["className"];
@@ -9,14 +11,40 @@ type ICardProps = {
 >;
 
 export const Card = ({ children, className, ...rest }: ICardProps) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const overlayRef = useRef<HTMLDivElement>(null);
+
+  const update = useCallback(({ x, y }: { x: number; y: number }) => {
+    if (!overlayRef.current) {
+      return;
+    }
+
+    const { width, height } = overlayRef.current?.getBoundingClientRect() ?? {};
+    const xOffset = x - width / 2;
+    const yOffset = y - height / 2;
+
+    overlayRef.current?.style.setProperty("--x", `${xOffset}px`);
+    overlayRef.current?.style.setProperty("--y", `${yOffset}px`);
+  }, []);
+
+  useMousePosition(containerRef, update);
+
   return (
     <div
-      className={twMerge(
-        "bg-card border-ring border transition-all duration-500 ease-in-out rounded-full backdrop-blur-[30px]",
+      ref={containerRef}
+      className={cn(
+        "group relative bg-card overflow-hidden border-ring border transition-all duration-500 ease-in-out rounded-full backdrop-blur-[30px] ",
         className
       )}
       {...rest}
     >
+      <div
+        ref={overlayRef}
+        className="z-10 absolute h-lg w-lg rounded-full bg-white opacity-0 bg-blend-soft-light blur-3xl transition-opacity group-hover:opacity-100"
+        style={{
+          transform: "translate(var(--x), var(--y))",
+        }}
+      />
       {children}
     </div>
   );
